3037db09b9c4f3325848c8972aae3b45273284df
queens kisivuli <queenskisivuli@gmail.com>
2023-08-22 20:36:55 +0300

    user migrations

---
+++ EFCoreOneToOneDemo/Migrations/20230821181008_InitialCreate.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreOneToOneDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Addresses_People_Id",
+                        column: x => x.Id,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
---
+++ EFCoreOneToOneDemo/Migrations/20230821185705_Schema.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreOneToOneDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class Schema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_People_Id",
+                table: "Addresses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_People",
+                table: "People");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses");
+
+            migrationBuilder.RenameTable(
+                name: "People",
+                newName: "Person, Schema = public");
+
+            migrationBuilder.RenameTable(
+                name: "Addresses",
+                newName: "Address, Schema = public");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Person, Schema = public",
+                table: "Person, Schema = public",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Address, Schema = public",
+                table: "Address, Schema = public",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Address, Schema = public_Person, Schema = public_Id",
+                table: "Address, Schema = public",
+                column: "Id",
+                principalTable: "Person, Schema = public",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Address, Schema = public_Person, Schema = public_Id",
+                table: "Address, Schema = public");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Person, Schema = public",
+                table: "Person, Schema = public");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Address, Schema = public",
+                table: "Address, Schema = public");
+
+            migrationBuilder.RenameTable(
+                name: "Person, Schema = public",
+                newName: "People");
+
+            migrationBuilder.RenameTable(
+                name: "Address, Schema = public",
+                newName: "Addresses");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_People",
+                table: "People",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_People_Id",
+                table: "Addresses",
+                column: "Id",
+                principalTable: "People",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
