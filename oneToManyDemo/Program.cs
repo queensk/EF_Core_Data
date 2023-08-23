e2bca0cd8a99c57dd5de0b6f091b0a5bf6074161
queens kisivuli <queenskisivuli@gmail.com>
2023-08-23 16:49:48 +0300

    main

--- oneToManyDemo/Program.cs
+++ oneToManyDemo/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+using oneToManyDemo.Models;
+using oneToManyDemo.Query;
+
+Person person = new Person(){
+    Name = "John",
+    PersonAddress = new Address(){
+        City = "New York",
+        State = "NY"
+    }
+};
+
+Query query = new Query();
+
+await query.CreatePerson(person);
